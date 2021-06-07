73dbdeca7b932968558f56b2743494880256f1ac
Cristian <camilo.santamaria322@gmail.com>
2021-06-07 14:44:44 -0500

    Se realizan las formas de crear un componente en react (constante, clase, funcion de javascript)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ function Helloworld()
   )
 }
 
+//Otra manera de crear un componente es por medio de una constante que creera una funcion de flecha la diferencia es que el return se obvia en este caso aunque se podria escribir 
+const App = () => <div>This is my component: <Helloworld /></div>
+
+//Otra forma de crearlo es por medio de una clase pero normalmente se hereda de react el component extendiendolo, el render me permite retornar ya que una clase no me permite hacerlo con su declaración
+class App extends React.Component {
+  render(){
+    return <div>This is my component: <Helloworld></Helloworld></div>
+  }
+}
+
 //Lo llamo las veces que lo necesite, mas adelante se vera que se pueden cambiar sus propiedades con props
 function App() {
   return (
