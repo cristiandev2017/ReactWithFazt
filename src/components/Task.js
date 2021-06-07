cc871f3fb1509750d219493c03e39e065af012e0
Cristian <camilo.santamaria322@gmail.com>
2021-06-07 18:17:41 -0500

    Se agrega estilos desde el archivo Task.js en el se usa el operador ternario al igual que como estilizar con constantes dando un entendimiento de como funciona esto en react

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,19 +1,40 @@
 import React, { Component } from "react";
 
 class Task extends Component {
+
+  StyleCompleted(){
+    return{
+      fontSize: '20px',
+      color: this.props.task.done ?'gray':'black',
+      textDecoration: this.props.task.done ? 'line-through':'none'
+    }
+  }
+
+
   render() {
     //Realizo destructuring evitando escribir varias veces la variable
     const { task } = this.props;
+    //const redColor = { background: "red" };
 
     return (
-      <div>
+      <p style={this.StyleCompleted()}>
         {task.title} - {task.description}
         {task.done} - {task.id}
         <input type="checkbox" />
-        <button>x</button>
-      </div>
+        <button style={btnDelete}>x</button>
+      </p>
     );
   }
 }
+
+const btnDelete = {
+  fontSize: "18px",
+  background: "#ea2027",
+  color: "#fff",
+  border: "none",
+  padding: "10px 15px",
+  borderRadius: '50%',
+  cursor: "pointer"
+};
 
 export default Task;
