8a791263428dc7159391d63698b19c5b6c6cdfe7
Cristian <camilo.santamaria322@gmail.com>
2021-06-07 16:24:20 -0500

    Manejo de los estados (State) y como utilizarlos en los contextos, uso del boton para su cambio por medio de funciones :)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+import React from "react";
 import "./App.css";
 
-//Se define como props para utilizar las propiedades.
-function Helloworld(props) {
-  //Probaremos por console log como recibimos los props
-  console.log(props);
-  return (
-    //me lo trae como un objeto y luego de esto me permite acceder a el en este caso solo tendremos de props mytext y tambien subtitle
-    <div id="hello">
-      <h3>{props.subtitle}</h3>
-      {props.mytext}
-    </div>
-  );
+class Helloworld extends React.Component {
+  //Empezaremos a usar state, el cual me permite almacenar datos
+  state = {
+    //Nos permite ocultar y mostrar datos
+    show: true,
+  };
+
+  //Se creara un metodo en la clase
+  toggleShow= () =>{
+    this.setState({show:!this.state.show})
+  }
+
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{this.props.subtitle}</h3>
+          {this.props.mytext}
+          {/* Aca se agrega un evento para cambiar por medio de state */}
+          <button onClick={this.toggleShow}>Toggle Show</button>
+        </div>
+      );
+    }else{
+      return <h1>There are not elements <button onClick={this.toggleShow}>Toggle Show</button> </h1>
+              
+    }
+  }
 }
 
 //Lo llamo las veces que lo necesite, mas adelante se vera que se pueden cambiar sus propiedades con props
@@ -20,8 +37,8 @@ function App() {
     <div>
       This is my component:{" "}
       <Helloworld mytext="Hello Cristian" subtitle="loremp ispum" />
-      <Helloworld mytext="Hola Mundo" subtitle="Otro subtitulo"/>
-      <Helloworld mytext="Hola Cristian" subtitle="Tercer subtitulo"/>
+      <Helloworld mytext="Hola Mundo" subtitle="Otro subtitulo" />
+      <Helloworld mytext="Hola Cristian" subtitle="Tercer subtitulo" />
     </div>
   );
 }
