8720bc3859874028f59312072d74ddce44413c3d
Cristian <camilo.santamaria322@gmail.com>
2021-06-07 20:17:59 -0500

    Se agrega consumo de api por medio del fakeapi de jsonplaceholder se agrega un nuevo componente para simular este comportamiento

--- src/App.js
+++ src/App.js
@@ -6,45 +6,50 @@ import tasks from "./sample/tasks.json";
 //Components
 import Tasks from "./components/Tasks";
 import TaskForm from "./components/TaskForm";
+import Posts from "./components/Posts";
 
 class App extends Component {
   state = {
     tasks: tasks,
   };
 
-  addTask = (title, description) =>{
+  addTask = (title, description) => {
     const newTask = {
       title: title,
       description: description,
-      id: this.state.tasks.length
-    }
+      id: this.state.tasks.length,
+    };
     this.setState({
-      tasks:[...this.state.tasks,newTask]
-    })
-  }
+      tasks: [...this.state.tasks, newTask],
+    });
+  };
 
-  deleteTask = (id) =>{
+  deleteTask = (id) => {
     //Se utiliza filter para traer los que no correspondan al id que estamos mandando
-    const newTasks = this.state.tasks.filter(task => task.id !== id)
-    this.setState({tasks:newTasks})
-  }
+    const newTasks = this.state.tasks.filter((task) => task.id !== id);
+    this.setState({ tasks: newTasks });
+  };
 
-  checkDone = (id) =>{
-    const newTasks = this.state.tasks.map(task=>{
-      if(task.id===id)
-      {
-        task.done = !task.done
+  checkDone = (id) => {
+    const newTasks = this.state.tasks.map((task) => {
+      if (task.id === id) {
+        task.done = !task.done;
       }
       return task;
     });
-    this.setState({tasks: newTasks})
-  }
+    this.setState({ tasks: newTasks });
+  };
 
   render() {
     return (
       <div>
-        <TaskForm addTask={this.addTask}/>
-        <Tasks tasks={this.state.tasks} deleteTask={this.deleteTask} checkDone={this.checkDone}/>
+        <TaskForm addTask={this.addTask} />
+        <Tasks
+          tasks={this.state.tasks}
+          deleteTask={this.deleteTask}
+          checkDone={this.checkDone}
+        />
+        <Posts />
       </div>
     );
   }
